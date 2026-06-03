298f76e4336e1a160c03eb86b0936792a469eed7
ds-gits-it <talentedmrl0real@gmail.com>
2026-06-03 18:14:14 +0530

    fix | update import path for styles in Separator component

--- src/components/Separator/Separator.jsx
+++ src/components/Separator/Separator.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from "./Separator.module.css";
+import styles from "./styles.module.css";
 const index = () => {
   return (
     <hr className={styles.separator}/>
